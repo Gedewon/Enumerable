1cc932bfb9ec4481ee1c297bd8b93b1a13b18858
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-07-22 23:02:19 +0530

    Add any method

--- index.rb
+++ index.rb
@@ -1,9 +1,5 @@
 module MyEnumerable
 
-    def each(list)
-
-    end
-
     def all?
         is_true= true
         list_index = 0
@@ -13,21 +9,35 @@ module MyEnumerable
         end
         is_true
     end
+
+    def any?
+        is_true = false
+        each do |e|
+          return true if yield e  #code condition
+        end
+        is_true
+    end
+
 end
 class MyList 
     include MyEnumerable
     attr_accessor :list
 
-    def initialize(list)
+    def initialize(*list)
         @list = list
     end
 
     def each
-        
+       @i = 0
+       while @i < list.length
+        yield @list[@i]
+        @i += 1
+       end
     end
-
-   
 end
 list = MyList.new([1, 2, 3, 4])
-puts  list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
+# puts  list.all? {|e| e < 5}
+# puts list.all? {|e| e > 5}
+puts list.any? {|e| e == 2} #true
+puts list.any? {|e| e == 5} #false
+
